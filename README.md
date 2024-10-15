66dbdd4bf92f6d16e9a7d987b66c14b2e438c994
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-10-16 01:36:32 +0530

    SupportingScripts:: integration of web scraping into ipynb generator

--- README.md
+++ README.md
@@ -5,11 +5,16 @@ Learn and practice JavaScript Algorithms and Data Structures through FreeCodeCam
 ## References: 
 
 1. [FreeCodeCamp JS algorithms and Data structures](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/)
+2. Small learning on web scraping using python-selenium: <https://www.scrapingbee.com/blog/selenium-python/>. Had to install `python3-selenium`. Also `BeautifulSoup` is used.
 
 
 ## Environment Setup:
 
 `ipynbGenerator.py` script shall be updated to latest project and run to get the project specific jupyter notebook. Thus, generated notebook will be just an outline. You shall update the project specific jupyter notebook.
+
+The idea is to generate the project specific README.md file using project specific jupyter notebook using the following command form:
+
+- `jupyter nbconvert <projectName>.ipynb --to markdown --no-input --output README.md`. Here `--no-input` flag indicates that in the generated markdown file code cells of the notebook will not be present. With this command you will get the project specific readme markdown file.
 
 **Note:** Using Node.js to run the scripts, since we need to run some/most of the javascript files without using them in html files
 
--- supportingScripts/ipynbGenerator.py
+++ supportingScripts/ipynbGenerator.py
@@ -1,6 +1,50 @@
 import nbformat
 from pathlib import Path
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-images')
+
+def webScrapJob(stepNo):
+    elements = []
+    driver = webdriver.Chrome(options=options)
+
+    url = f"https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/learn-introductory-javascript-by-building-a-pyramid-generator/step-{stepNo}"  # UPDATE HERE
+
+    driver.get(url)
+    element = ""
+    foundItems = ""
+
+    try:
+        print(f"Trying to scrape step{stepNo}")
+        element = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.ID, 'description')))
+        foundItems = element.get_attribute('innerHTML')
+    finally:
+        driver.quit()
+
+    if element:
+        # print(element)
+        # print(foundItems)
+        soup = BeautifulSoup(foundItems, 'html.parser')
+
+        textContent = ""
+        entries = soup.findAll('p')
+
+        for entry in entries:
+            # print(entry.text)
+            # elements.append(entry.text)
+            elements.append(entry)
+        # print(entries)
+        print(f"Successfully scraped step{stepNo}")
+        return elements
+
 jnb = nbformat.NotebookNode()
 
 jnb['nbformat'] = 4
@@ -28,24 +72,44 @@ jnb['metadata'] = {
 
 jnb['cells'] = []
 
+
 cell = nbformat.NotebookNode(cell_type='code', metadata={}, outputs=[], execution_count=0, source=["from IPython.display import display, Markdown, HTML, Image"])
 jnb['cells'].append(cell)
 
-projectTitle = "# " + "Title of the project"
-projectName = "pyramidGenerator"
-number_of_steps = 4
+projectIntro = """JavaScript is a powerful scripting language that you can use to make web pages interactive. It's one of the core technologies of the web, along with HTML and CSS. All modern browsers support JavaScript.
+
+In this practice project, you'll learn fundamental programming concepts in JavaScript by coding your own Pyramid Generator. You'll learn how to work with arrays, strings, functions, loops, if/else statements, and more."""  # UPDATE HERE (Copy paste from freeCodeCamp website course page)
+projectTitle = "# " + "Learn Introductory JavaScript by Building a Pyramid Generator"  # UPDATE HERE
+projectName = "pyramidGenerator"  # UPDATE HERE
+number_of_steps = 118  # UPDATE HERE
 projectPath = "../" + projectName
 
 cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=[f"{projectTitle}"])
 jnb['cells'].append(cell)
 
+cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=[f"{projectIntro}"])
+jnb['cells'].append(cell)
+
+
+print(f"============{projectName}============")
 
 for i in range(1, number_of_steps + 1):
+    # START: Webscraping
+    elements = []
+    elements = webScrapJob(i)
+    # END: Webscraping
+
     srcFileName = f"{projectName}_step{i}.js"
 
     Path(f"{projectPath}/{srcFileName}").touch()
 
-    cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=[f"## Step{i}:", "\n", "\n", "\n", f"**Source file link:** [{srcFileName}](./{srcFileName})", "\n", "\n"])
+    cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=[f"## Step{i}:", "\n"])
+    jnb['cells'].append(cell)
+    for element in elements:
+        cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=[f"{element}"])
+        jnb['cells'].append(cell)
+
+    cell = nbformat.NotebookNode(cell_type='markdown', metadata={}, source=["\n", "\n", f"**Source file link:** [{srcFileName}](./{srcFileName})", "\n", "\n"])
     jnb['cells'].append(cell)
 
     cell = nbformat.NotebookNode(cell_type='code', metadata={}, outputs=[], execution_count=0, source=["code = \"\"\n", f"fileName = \"{srcFileName}\"\n", "\n", "with open(fileName, \"r\") as file:", "\tcode = file.read()", "\n", "display(Markdown(f\"\"\"```js\n{code}\n```\"\"\"))"])
@@ -66,3 +130,6 @@ fname = f"{projectPath}/{projectName}.ipynb"
 
 with open(fname, 'w') as f:
     nbformat.write(jnb, f)
+
+print(f"============{projectName}============")
+print('DONE: ipynb generation script')
