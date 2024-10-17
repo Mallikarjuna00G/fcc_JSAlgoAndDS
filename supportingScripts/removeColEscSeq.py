ce85d93ae5c0db050c1f60adefe9dfa7f91c66ec
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-10-17 13:42:01 +0530

    SupportingScripts:: script to remove the color escape sequences that are added in README by jupyter for output cells

---
+++ supportingScripts/removeColEscSeq.py
@@ -0,0 +1,24 @@
+import fileinput
+
+projectName = "pyramidGenerator"  # UPDATE HERE
+projectPath = "../" + projectName
+
+lookFor = [
+    "[30m",
+    "[31m",
+    "[32m",
+    "[33m",
+    "[34m",
+    "[35m",
+    "[36m",
+    "[37m",
+    "[38m",
+    "[39m",
+    "[90m",
+]
+
+with fileinput.input(f"{projectPath}/README.md", inplace=True) as file:
+    for line in file:
+        for i in lookFor:
+            line = line.replace(i, "")
+        print(line, end='')
