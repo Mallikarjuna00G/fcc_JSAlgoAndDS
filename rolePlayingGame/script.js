d8996c84eff63bd06c8638ff060f157f3863b233
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-10-18 17:21:12 +0530

    rolePlayingGame::Step109

--- rolePlayingGame/script.js
+++ rolePlayingGame/script.js
@@ -93,16 +93,25 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if(gold >= 30) {
-        gold -= 30;
-        currentWeaponIndex++;
-        goldText.innerText = gold;
-        let newWeapon = weapons[currentWeaponIndex].name;
-        text.innerText = "You now have a "+ newWeapon + ".";
-
+    if(currentWeaponIndex < weapons.length - 1) {
+        if(gold >= 30) {
+            gold -= 30;
+            currentWeaponIndex++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeaponIndex].name;
+            text.innerText = "You now have a "+ newWeapon + ".";
+    
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have " + inventory;
+        }
+        else {
+            text.innerText = "You do not have enough gold to buy a weapon."
+        }
     }
     else {
-        text.innerText = "You do not have enough gold to buy weapon."
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
 }
 
@@ -124,4 +133,17 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function sellWeapon() {
+    if(inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currenWeapon = inventory.shift();
+        text.innerText = "You sold a " + currenWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    }
+    else {
+        text.innerText = "Don't sell your only weapon!";
+    }
 }
