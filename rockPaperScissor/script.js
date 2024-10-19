1570300bd04db135d997bee907eed2e3d5f4c8da
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-10-20 04:52:49 +0530

    rockPaperScissor::step2

--- rockPaperScissor/script.js
+++ rockPaperScissor/script.js
@@ -1,5 +1,24 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-    return options[Math.floor(Math.random() * 10) % 3];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+  
+function hasPlayerWonTheRound(player, computer) {    
+    if(
+        (player === "Rock" && computer === "Paper") ||
+        (player === "Paper" && computer === "Scissors") ||
+        (player === "Scissors" && computer === "Rock") ||
+        (player === computer)
+    ) {
+        return false;
+    }
+    return true;
+}
+  
+console.log(hasPlayerWonTheRound("Rock", "Paper")); 
+console.log(hasPlayerWonTheRound("Paper", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+console.log(hasPlayerWonTheRound("Rock", "Rock")); 
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
